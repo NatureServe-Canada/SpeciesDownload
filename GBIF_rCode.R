250bcc5e0885c6cee142e8d28e558fc3c0345a29
Gabrielle Miller <43541980+gmlmiller@users.noreply.github.com>
2021-06-01 12:32:45 -0500

    Update GBIF_rCode.R
    Changes to accomodate updates to dplyr.

--- GBIF_rCode.R
+++ GBIF_rCode.R
@@ -1,8 +1,7 @@
 #This code uses r packages rgbif: https://cran.r-project.org/web/packages/rgbif/index.html and tidyverse: https://www.tidyverse.org/
 
 #Install packages
-install.packages("rgbif")
-install.packages("tidyverse")
+install.packages("rgbif", "tidyverse")
 
 #Load packages 
 library(rgbif)
@@ -12,27 +11,20 @@ library(tidyverse)
 list_sciNames <- c("Bombus bohemicus","Bombus occidentalis","Bombus suckleyi","Somatochlora septentrionalis","Germaria angustata","Stagnicola kennicotti")
 
 #Search for the taxonKeys of the species listed above, including subpsecies and varities
-keys <- lapply(list_sciNames, function(x) name_suggest(x, rank=c("species", "subspecies", "variety"))) %>%
+keys <- sapply(list_sciNames, function(x) name_suggest(x, rank=c("species", "subspecies", "variety")))%>%
   bind_rows()%>%
   select(key)%>%
   unlist(use.names = F)
 
 #Search for the GBIF occurrences for the species listed above (*Note make sure you review the limit (default is 500, hard maximum is 100,000) based on the number of occurrences likely to be returned*) 
-GBIF_occ <- occ_search(taxonKey = keys, return="data", hasCoordinate = TRUE, limit= 60000, fields=c('occurrenceID','gbifID','scientificName', 'locality', 'stateProvince', 'individualCount','decimalLatitude','decimalLongitude','coordinateUncertaintyInMeters','day','month','year','basisOfRecord','institutionCode','identifiedBy','dateIdentified','license','recordedBy','issues','references','geodeticDatum','countryCode','informationWithheld','verbatimLocality','occurrenceRemarks','species'))
-
-#Identify taxonKeys that returned GBIF occurrences  
-GBIF_names <- GBIF_occ %>%
-  sapply(., nrow) %>%
-  unlist(.) %>%
-  names(.)
-
-#Merge data into single table for export
-GBIF_data <- GBIF_occ %>%
-  .[GBIF_names] %>%
-  do.call(bind_rows, .)
+GBIF_occ <- occ_search(taxonKey = keys, hasCoordinate = TRUE, limit= 60000, fields=c('occurrenceID','gbifID','scientificName', 'locality', 'stateProvince', 'individualCount','decimalLatitude','decimalLongitude','coordinateUncertaintyInMeters','day','month','year','basisOfRecord','institutionCode','identifiedBy','dateIdentified','license','recordedBy','issues','references','geodeticDatum','countryCode','informationWithheld','verbatimLocality','occurrenceRemarks','species'))
+  
+#Merge data into single table for export (*Note: taxonKeys that returned no data are automatically omitted from the table)
+GBIF_data <- lapply(GBIF_occ, `[[`, "data")%>%
+  bind_rows()
 
 #Remove Fossil records
-GBIF_sub <-subset(GBIF_data, GBIF_data$basisOfRecord != "FOSSIL_SPECIMEN")
+GBIF_sub <-GBIF_data[(GBIF_data$basisOfRecord != "FOSSIL_SPECIMEN"),]
 
 #Export as csv to specified folder
 write.csv(GBIF_sub,"C:/Desktop/GBIF_Data.csv")
